fe8c397c360be0f67152f5e58533cb26fb30b719
Anjan Roy <anjanroy@yandex.com>
2021-01-19 15:53:51 +0530

    concurrent implementation of serialize protobuf & write to file

--- main.go
+++ main.go
@@ -17,4 +17,12 @@ func main() {
 		log.Printf("[+] Wrote %d protocol buffer encoded entries in %s [ Sequential ]\n", count, end.Sub(start))
 	}
 
+	start = time.Now()
+	ret = ConcurrentWriteAllToFile(file, count)
+	end = time.Now()
+
+	if ret {
+		log.Printf("[+] Wrote %d protocol buffer encoded entries in %s [ Concurrent ]\n", count, end.Sub(start))
+	}
+
 }
